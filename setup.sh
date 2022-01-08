e389e79996dec6bd7f5e01225bd5d1e3e3f1b99b
SG LT 2020 <gsamarth97@gmail.com>
2022-01-08 10:50:41 +0530

    ProcFile updated to use Streamlit

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# This is used for Streamlit App Build process, by Heroku
+mkdir -p ~/.streamlit/
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
